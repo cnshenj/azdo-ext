664227fcb960aa48d92a5bae5717ffc52271fc67
Zhang Shen <zhangs@microsoft.com>
2022-08-17 14:06:53 -0700

    Initial commit

---
+++ build.mjs
@@ -0,0 +1,39 @@
+﻿import esbuild from "esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
+import { copy, remove } from "fs-extra";
+import path from "path";
+
+await remove("dist");
+await copy("src/views", "dist");
+
+/** @type { import("esbuild").BuildOptions }  */
+const buildOptions = {
+  bundle: true,
+  entryPoints: [
+    "src/index.tsx"
+  ],
+  outdir: "dist",
+  loader: {
+    ".woff": "copy"
+  },
+  plugins: [
+    {
+      name: "Logger",
+      setup(build) {
+        build.onResolve({ filter: /(?:FabricIcons\.css|AzDevMDL2)/ }, async (args) => {
+          console.log(args.path);
+          return { path: path.resolve(args.resolveDir, args.path) };
+        });
+      }
+    },
+    sassPlugin({ type: "style" })
+  ]
+};
+
+/** @type { import("esbuild").ServeOptions }  */
+const serveOptions = {
+  host: "localhost",
+  servedir: "dist"
+};
+
+await esbuild.build(buildOptions);
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+﻿import { Icon } from "azure-devops-ui/Icon";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss"
+
+const container = document.getElementById("app");
+const root = createRoot(container!);
+root.render(<App />);
+
+function App(): JSX.Element {
+  return (
+    <Icon iconName="Folder" />
+  );
+}
